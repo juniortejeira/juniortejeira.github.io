84c2f040abb98ec12276e092a99090e5fa9ee8a7
juniortejeira <101424705+juniortejeira@users.noreply.github.com>
2022-10-06 16:54:30 -0300

    Add files via upload

---
+++ build/precache-manifest.f46e92173aee9c7228cafc3efb566915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760e3a0e2b9b163820053e22b5266a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df91c83accc9297e6303",
+    "url": "/static/css/main.65988eb9.chunk.css"
+  },
+  {
+    "revision": "292ecc43616706572996",
+    "url": "/static/js/2.decc470d.chunk.js"
+  },
+  {
+    "revision": "83b3628a623953748de98059f50ba3ca",
+    "url": "/static/js/2.decc470d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df91c83accc9297e6303",
+    "url": "/static/js/main.3d6b61c4.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
